84a0f25914833df822ed810e41e1ed929fd1a652
disabled1212 <daniil.fedotov2004@gmail.com>
2023-05-19 15:14:30 +0300

    second commit

---
+++ zadanie 5 yslovie/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace zadanie_5_yslovie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int num = Convert.ToInt16(Console.ReadLine());
+            switch (num)
+            {
+                case 1: Console.WriteLine("Понедельник");break;
+                case 2: Console.WriteLine("Вторник"); break;
+                case 3: Console.WriteLine("Среда"); break;
+                case 4: Console.WriteLine("Четверг"); break;
+                case 5: Console.WriteLine("Пятница"); break;
+                case 6: Console.WriteLine("Суббота"); break;
+                case 7: Console.WriteLine("Воскресенье"); break;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
